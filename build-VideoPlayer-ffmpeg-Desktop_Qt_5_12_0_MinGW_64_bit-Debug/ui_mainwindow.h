39a3f022468150880d664260157cf4a1003bd4f5
Yoon Dombo <dombo@qq.com>
2023-03-26 23:47:41 +0800

    update  player UI
    autolayout player UI

--- build-VideoPlayer-ffmpeg-Desktop_Qt_5_12_0_MinGW_64_bit-Debug/ui_mainwindow.h
+++ build-VideoPlayer-ffmpeg-Desktop_Qt_5_12_0_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -11,8 +11,14 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSlider>
+#include <QtWidgets/QSpacerItem>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QToolBar>
 #include <QtWidgets/QWidget>
@@ -23,6 +29,18 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralWidget;
+    QGridLayout *gridLayout;
+    QLabel *label;
+    QSlider *horizontalSlider;
+    QWidget *widget;
+    QHBoxLayout *horizontalLayout;
+    QPushButton *pushButton;
+    QSpacerItem *horizontalSpacer;
+    QPushButton *pushButton_2;
+    QSpacerItem *horizontalSpacer_2;
+    QPushButton *pushButton_3;
+    QSpacerItem *horizontalSpacer_3;
+    QPushButton *pushButton_4;
     QMenuBar *menuBar;
     QToolBar *mainToolBar;
     QStatusBar *statusBar;
@@ -34,6 +52,70 @@ public:
         MainWindow->resize(903, 645);
         centralWidget = new QWidget(MainWindow);
         centralWidget->setObjectName(QString::fromUtf8("centralWidget"));
+        gridLayout = new QGridLayout(centralWidget);
+        gridLayout->setSpacing(6);
+        gridLayout->setContentsMargins(11, 11, 11, 11);
+        gridLayout->setObjectName(QString::fromUtf8("gridLayout"));
+        label = new QLabel(centralWidget);
+        label->setObjectName(QString::fromUtf8("label"));
+        label->setAlignment(Qt::AlignCenter);
+
+        gridLayout->addWidget(label, 0, 0, 1, 1);
+
+        horizontalSlider = new QSlider(centralWidget);
+        horizontalSlider->setObjectName(QString::fromUtf8("horizontalSlider"));
+        horizontalSlider->setOrientation(Qt::Horizontal);
+
+        gridLayout->addWidget(horizontalSlider, 1, 0, 1, 1);
+
+        widget = new QWidget(centralWidget);
+        widget->setObjectName(QString::fromUtf8("widget"));
+        widget->setMaximumSize(QSize(16777215, 40));
+        horizontalLayout = new QHBoxLayout(widget);
+        horizontalLayout->setSpacing(6);
+        horizontalLayout->setContentsMargins(11, 11, 11, 11);
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        pushButton = new QPushButton(widget);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+        pushButton->setMinimumSize(QSize(0, 30));
+        pushButton->setMaximumSize(QSize(16777215, 50));
+        pushButton->setIconSize(QSize(16, 16));
+
+        horizontalLayout->addWidget(pushButton);
+
+        horizontalSpacer = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout->addItem(horizontalSpacer);
+
+        pushButton_2 = new QPushButton(widget);
+        pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
+        pushButton_2->setMinimumSize(QSize(0, 30));
+
+        horizontalLayout->addWidget(pushButton_2);
+
+        horizontalSpacer_2 = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout->addItem(horizontalSpacer_2);
+
+        pushButton_3 = new QPushButton(widget);
+        pushButton_3->setObjectName(QString::fromUtf8("pushButton_3"));
+        pushButton_3->setMinimumSize(QSize(0, 30));
+
+        horizontalLayout->addWidget(pushButton_3);
+
+        horizontalSpacer_3 = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout->addItem(horizontalSpacer_3);
+
+        pushButton_4 = new QPushButton(widget);
+        pushButton_4->setObjectName(QString::fromUtf8("pushButton_4"));
+        pushButton_4->setMinimumSize(QSize(0, 30));
+
+        horizontalLayout->addWidget(pushButton_4);
+
+
+        gridLayout->addWidget(widget, 2, 0, 1, 1);
+
         MainWindow->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(MainWindow);
         menuBar->setObjectName(QString::fromUtf8("menuBar"));
@@ -54,6 +136,11 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", nullptr));
+        label->setText(QApplication::translate("MainWindow", "\345\212\240\346\262\271\345\225\212\357\274\201\357\274\201\357\274\201\357\274\201\360\237\230\200", nullptr));
+        pushButton->setText(QApplication::translate("MainWindow", "\346\211\223\345\274\200", nullptr));
+        pushButton_2->setText(QApplication::translate("MainWindow", "\346\222\255\346\224\276", nullptr));
+        pushButton_3->setText(QApplication::translate("MainWindow", "\346\232\202\345\201\234", nullptr));
+        pushButton_4->setText(QApplication::translate("MainWindow", "\345\201\234\346\255\242", nullptr));
     } // retranslateUi
 
 };
