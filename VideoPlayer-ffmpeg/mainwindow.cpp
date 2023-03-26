47aaa4c5dcdd8034f0b55bf06f2d509390537a7a
Yoon Dombo <dombo@qq.com>
2023-03-27 00:07:16 +0800

    add logic and file format screening of the opening file
    Add the logic and file format screening of the opening file

--- VideoPlayer-ffmpeg/mainwindow.cpp
+++ VideoPlayer-ffmpeg/mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QDebug>
+#include <QFileDialog>
+
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -19,6 +21,16 @@ MainWindow::~MainWindow()
 void MainWindow::on_pushButton_clicked()
 {
     qDebug()<<"aaaaaaaa";
+    QStringList filenames  = QFileDialog::getOpenFileNames(this,"看看电影","C://Users/Administrator\Desktop",
+                                                           "allfiles(*.*)"
+                                                           "MP3(*.mp3);;"
+                                                           "MP4(*.mp4);;"
+                                                           "MKV(*mkv);;"
+                                                           "AVI(*.AVI)");
+//    QString filenames = QFileDialog::getOpenFileName(this,tr("选择文件"), "/", "All files (*)");
+    foreach(QString const & str,filenames){
+        qDebug() << str << "on_pushButton_clicked";
+    }
 }
 
 void MainWindow::on_pushButton_2_clicked()
